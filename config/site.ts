bd2c94abd69f97a9775c6238a6ac4b11f67f8ca7
Зайков Евгений <zaijenia@gmail.com>
2023-06-16 00:53:17 +0500

    initial

--- config/site.ts
+++ config/site.ts
@@ -1,18 +1,12 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Next.js",
-  description:
-    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+  name: "Кориолис",
+  description: "Просто кориолис",
   mainNav: [
     {
-      title: "Home",
-      href: "/",
+      title: "Опыт",
+      href: "/experience",
     },
   ],
-  links: {
-    twitter: "https://twitter.com/shadcn",
-    github: "https://github.com/shadcn/ui",
-    docs: "https://ui.shadcn.com",
-  },
 }
